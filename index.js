192147598e5d4e8b153ce62c669eab04c23478c0
PostHTX <posthtx@gmail.com>
2020-04-20 23:50:28 -0500

    get time 2

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ s = t.getSeconds();
 
 const amPm = h > 12 ? 'PM' : 'AM'
 h = h % 12 || 12
-console.log(h,m,s,amPm)
+//console.log(h,m,s,amPm)
+
+time.innerHTML = `${h}:${m}:${s} ${amPm}`
 
 setTimeout(showTime,1000)
 }
