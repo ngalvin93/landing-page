9cefc91042f73373502c27c3f4597525e914c832
PostHTX <posthtx@gmail.com>
2020-04-20 23:37:59 -0500

    get time

---
+++ index.js
@@ -0,0 +1,16 @@
+const time = document.getElementById("time")
+
+function showTime () {
+let t = new Date(),
+h = t.getHours(),
+m = t.getMinutes(),
+s = t.getSeconds();
+
+const amPm = h > 12 ? 'PM' : 'AM'
+h = h % 12 || 12
+console.log(h,m,s,amPm)
+
+setTimeout(showTime,1000)
+}
+
+showTime()
